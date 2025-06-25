ea9f8b9c50b6e4297bac4b39c01c410eebe88bc3
Bimix12 <walidettrich2n@gmail.com>
2025-06-25 21:26:02 +0100

    Create page.tsx

---
+++ src/app/number/[number]/page.tsx
@@ -0,0 +1,5 @@
+// This page is no longer used as messages are displayed inline on the main page.
+// You can delete this file if you no longer need a separate page for numbers.
+export default function NumberPage() {
+    return null;
+}
